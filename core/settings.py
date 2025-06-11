89e07b79d1c6e605db4072da5b4ec67ed85212f2
João Rafael <joaormbarbosa@gmail.com>
2025-06-11 07:31:13 -0300

    🔐 Remove SECRET_KEY do settings.py e usa variável de ambiente no Render

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vy7+f#)$m=m^ai!#!ja!m56xvbr6-a_$f7-bv8phq43n$-89y7'
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'chave-padrao-secreta-para-desenvolvimento')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
