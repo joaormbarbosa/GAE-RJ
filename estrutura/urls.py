b5203c9150d0fe14eabedda8f239bb2460c93e60
João Rafael <joaormbarbosa@gmail.com>
2025-06-14 16:18:11 -0300

    fix: adiciona estrutura/urls.py com rotas da aplicação

--- estrutura/urls.py
+++ estrutura/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('confirmar-sigilo/', views.confirmar_sigilo, name='confirmar_sigilo'),
+    path('checklist/', views.checklist_geral, name='checklist_geral'),
+    path('bi/', views.bi_dashboard, name='bi_dashboard'),
+    path('auditoria-log/', views.auditoria_log, name='auditoria_log'),
 ]
